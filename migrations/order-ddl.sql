037ab6069d69cba37704862284639600b584fc2f
TheNimaDev <TheNimaDev@gmail.com>
2025-11-15 19:15:31 +0330

    Add plan migration and update order migration

--- migrations/order-ddl.sql
+++ migrations/order-ddl.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS orders (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
-    plan VARCHAR(100) NOT NULL,
-    period_plan ENUM('7', '30','90') NOT NULL,
+    plan_id INT UNSIGNED NOT NULL,
     status ENUM('pending', 'reject', 'done') NOT NULL DEFAULT 'pending',
     chat_id BIGINT UNSIGNED NOT NULL,
     create_at BIGINT NOT NULL,
@@ -11,5 +10,8 @@ CREATE TABLE IF NOT EXISTS orders (
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (plan_id) REFERENCES plans(id)
+        ON DELETE RESTRICT
         ON UPDATE CASCADE
-) AUTO_INCREMENT = 10 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) AUTO_INCREMENT = 10

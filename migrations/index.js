4b42bf9afa055d9dc0b6aa5a47046da1e1de21d7
TheNimaDev <TheNimaDev@gmail.com>
2025-11-16 18:11:16 +0330

    Add confirm/reject command and data to db

--- migrations/index.js
+++ migrations/index.js
@@ -6,7 +6,7 @@ let migrate = async () => {
     let connection = await db.getConnection()
     try {
         let createUserTableSql = fs.readFileSync(path.join(__dirname, "./users-ddl.sql"),"utf-8")
-        let createOrdersTableSql = fs.readFileSync(path.join(__dirname, "./orders-ddl.sql"),"utf-8")
+        let createOrdersTableSql = fs.readFileSync(path.join(__dirname, "./order-ddl.sql"),"utf-8")
         let createPlansTableSql = fs.readFileSync(path.join(__dirname, "./plan-ddl.sql"),"utf-8")
         await connection.beginTransaction()
         connection.query(createPlansTableSql)

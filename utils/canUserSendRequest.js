417f82548b7cf5973ce03d0f92871adaddaf6b59
imNima10 <x1nima1x@gmail.com>
2025-11-20 15:27:16 +0330

    Add validation for increment free requests

--- utils/canUserSendRequest.js
+++ utils/canUserSendRequest.js
@@ -2,23 +2,25 @@ let keyboards = require("../keyboards")
 const { isUsersFreeRequestsFinished, isUserHasAccess } = require("../repos")
 
 module.exports = async (chatId, model) => {
+    let isFreeReq = true
+
     let hasAccess = await isUserHasAccess(chatId, model)
 
     if (hasAccess) {
-        return {
-            access: true
+        isFreeReq = false
+    } else {
+        let isUserReqFreeFinished = await isUsersFreeRequestsFinished(chatId)
+        if (isUserReqFreeFinished) {
+            return {
+                access: false,
+                message: "تعداد درخواست های رایگان شما تموم شده است!",
+                keyboard: keyboards.endFreeRequestMessage
+            }
         }
     }
 
-    let isUserReqFreeFinished = await isUsersFreeRequestsFinished(chatId)
-    if (isUserReqFreeFinished) {
-        return {
-            access: false,
-            message: "تعداد درخواست های رایگان شما تموم شده است!",
-            keyboard: keyboards.endFreeRequestMessage
-        }
-    }
     return {
-        access: true
+        access: true,
+        isFreeReq
     }
 }
